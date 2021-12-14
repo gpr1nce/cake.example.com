e53e7084c6cce9fd7adbbd81666b3f56f17ed7ef
George Prince <prince.george35@gmail.com>
2021-12-14 13:19:27 -0600

    Add POSTS CRUD

---
+++ tests/Fixture/PostsFixture.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * PostsFixture
+ */
+class PostsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => '4a373040-7fd3-499e-b140-ac6192cbf696',
+                'title' => 'Lorem ipsum dolor sit amet',
+                'slug' => 'Lorem ipsum dolor sit amet',
+                'meta_keywords' => 'Lorem ipsum dolor sit amet',
+                'meta_description' => 'Lorem ipsum dolor sit amet',
+                'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'user_id' => '6061e8e9-12d6-435e-ab26-a363ebdbc5c4',
+                'created' => '2021-12-14 19:06:14',
+                'modified' => '2021-12-14 19:06:14',
+            ],
+        ];
+        parent::init();
+    }
+}
